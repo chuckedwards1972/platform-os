8a0891150d4b767af10979f848c7185efaafaaa1
chuckedwards1972 <bro.chuck.edwards@me.com>
2026-04-13 15:46:12 -0500

    Serve POLR platform from public folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => { router.replace('/landing'); }, []);
+  useEffect(() => { window.location.href = '/polr.html'; }, []);
   return null;
 }
